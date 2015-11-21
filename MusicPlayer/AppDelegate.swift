420558b3a1a42af091d9aeb79e5e98d32db7d061
Kyle Minshall <kyleminshall@gmail.com>
2015-11-20 16:58:12 -0800

    Initial Commit

---
+++ MusicPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicPlayer
+//
+//  Created by Kyle Minshall on 11/20/15.
+//  Copyright © 2015 tmg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Kyle Minshall on 11/20/15.
+//  Copyright © 2015 tmg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
